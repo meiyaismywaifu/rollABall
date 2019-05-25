a1bae3c62a92e5109cc65f090fe3785d870eedd3
meiyaismywaifu <rokasomee@gmail.com>
2019-05-25 16:31:57 -0700

    unity extension test 2
    continuation

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player; // GameObject becomes a thing to reference inside Unity UI
+	// unclear what "player" does, doesn't change UI label, "Player" appeared anyways?
+	
+    private Vector3 offset;
+
+    void Start () // "initialization"?
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate () // same thing as update but just later? like priority setting?
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
